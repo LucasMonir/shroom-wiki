3545575779c6ac9821bc0d0a7f82bc13f3d18935
LucasMonir <lucasmonir@hotmail.com>
2023-05-31 11:07:04 -0300

    added style to the shroom card

--- src/components/pages/js/Mushroom.js
+++ src/components/pages/js/Mushroom.js
@@ -1,24 +1,27 @@
 import style from '../css/Mushroom.module.css'
 
 function Mushroom() {
-    const shroom_info = ['Genus', 'Species', 'Edible', 'Toxic']
-
     return (
         <section className={style.shroom_container}>
 
             <div className={style.shroom_data_container}>
                 <div className={style.shroom_image}>
+                    {/* <img src="https://cdn.britannica.com/90/236590-050-27422B8D/Close-up-of-mushroom-growing-on-field.jpg" alt="" /> */}
                 </div>
                 <div className={style.shroom_data}>
-                    <h1>Amanita muscaria</h1>
-                    <div>
+                    <h1 className={style.mushroom_name}>Amanita muscaria</h1>
+                    <div className={style.mushroom_items}>
                         <ul>
-                            {shroom_info.map((x) => <li>{x}:</li>)}
+                            <li className={style.shroom_item}>Genus: <span className={style.mushroom_info}>Amanita</span></li>
+                            <li className={style.shroom_item}>Species: <span className={style.mushroom_info}>Muscária</span></li>
+                            <li className={style.shroom_item}>Edible: <span className={style.mushroom_info}>No</span></li>
+                            <li className={style.shroom_item}>Toxic: <span className={style.mushroom_info}>Yes</span></li>
                         </ul>
-                        amanita musgasaospdkaspo paoskposad opasdasopdk pak
-                        sdopasdopkasdko asdopsadpo askopdpokaspokdas opaposdopasd
-                        aspod  aposkdpoadsk.
-                        asdopdopaskopd sapo kd
+
+                        <p className={style.mushroom_description}>
+                            Amanita muscaria,Amanita muscaria,AAmanita muscaria,Amanita muscaria,Amanita muscaria,Amanita muscaria,Amanita muscaria,Amanita muscaria,Amanita muscaria,Amanita muscaria,manita muscaria,Amanita muscaria,Amanita muscaria,Amanita muscaria,Amanita muscaria,Amanita muscaria,Amanita muscaria,Amanita muscaria,Amanita muscaria,Amanita muscaria,Amanita muscaria,Amanita muscaria,Amanita muscaria,Amanita muscaria,Amanita muscaria,Amanita muscaria,Amanita muscaria,Amanita muscaria,Amanita muscaria,Amanita muscaria,Amanita muscaria,Amanita muscaria,Amanita muscaria,Amanita muscaria,Amanita muscaria,Amanita muscaria,Amanita muscaria,Amanita muscaria,Amanita muscaria,Amanita muscaria,Amanita muscaria,Amanita muscaria,Amanita muscaria, commonly known as the fly agaric or fly amanita,[5] is a basidiomycete of the genus Amanita. It is also a muscimol mushroom.[6] Native throughout the temperate and boreal regions of the Northern Hemisphere, Amanita muscaria has been unintentionally introduced to many countries in the Southern Hemisphere, generally as a symbiont with pine and birch plantations, and is now a true cosmopolitan species. It associates with various deciduous and coniferous trees.
+                        </p>
+
                     </div>
                 </div>
             </div>
