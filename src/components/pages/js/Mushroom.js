cdf616cc18c5c566fd0c705bd7d348ac63f290d7
LucasMonir <lucasmonir@hotmail.com>
2023-05-31 15:23:51 -0300

    added box shadow and minor fixes

--- src/components/pages/js/Mushroom.js
+++ src/components/pages/js/Mushroom.js
@@ -21,7 +21,6 @@ function Mushroom() {
                         <p className={style.mushroom_description}>
                             Amanita muscaria,Amanita muscaria,AAmanita muscaria,Amanita muscaria,Amanita muscaria,Amanita muscaria,Amanita muscaria,Amanita muscaria,Amanita muscaria,Amanita muscaria,manita muscaria,Amanita muscaria,Amanita muscaria,Amanita muscaria,Amanita muscaria,Amanita muscaria,Amanita muscaria,Amanita muscaria,Amanita muscaria,Amanita muscaria,Amanita muscaria,Amanita muscaria,Amanita muscaria,Amanita muscaria,Amanita muscaria,Amanita muscaria,Amanita muscaria,Amanita muscaria,Amanita muscaria,Amanita muscaria,Amanita muscaria,Amanita muscaria,Amanita muscaria,Amanita muscaria,Amanita muscaria,Amanita muscaria,Amanita muscaria,Amanita muscaria,Amanita muscaria,Amanita muscaria,Amanita muscaria,Amanita muscaria,Amanita muscaria, commonly known as the fly agaric or fly amanita,[5] is a basidiomycete of the genus Amanita. It is also a muscimol mushroom.[6] Native throughout the temperate and boreal regions of the Northern Hemisphere, Amanita muscaria has been unintentionally introduced to many countries in the Southern Hemisphere, generally as a symbiont with pine and birch plantations, and is now a true cosmopolitan species. It associates with various deciduous and coniferous trees.
                         </p>
-
                     </div>
                 </div>
             </div>
