58b9b1721385c31a6ad2ba2df5279a3b1e278571
Lucas <lucasmonir@hotmail.com>
2023-06-02 11:46:27 -0300

    added some logic for the random shroom feature, need to add toxic and edible properties on the database (backend)

--- src/components/pages/js/Mushroom.js
+++ src/components/pages/js/Mushroom.js
@@ -1,6 +1,27 @@
 import style from '../css/Mushroom.module.css'
+import { useEffect, useState } from "react";
 
-function Mushroom() {
+function Mushroom({ id }) {
+    const [shroom, setShroom] = useState();
+
+    useEffect(() => {
+        if (id) {
+
+            fetch('http://localhost:4200/randomShroom',
+                {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+
+                })
+                .then(x => x.json())
+                .then((x) => { setShroom(x) })
+                .catch(err => console.log(err))
+        }
+    })
+
+
     return (
         <section className={style.shroom_container}>
 
@@ -9,11 +30,11 @@ function Mushroom() {
                     {/* <img src="https://cdn.britannica.com/90/236590-050-27422B8D/Close-up-of-mushroom-growing-on-field.jpg" alt="" /> */}
                 </div>
                 <div className={style.shroom_data}>
-                    <h1 className={style.mushroom_name}>Amanita muscaria</h1>
+                    <h1 className={style.mushroom_name}>{shroom.name}</h1>
                     <div className={style.mushroom_items}>
                         <ul>
-                            <li className={style.shroom_item}>Genus: <span className={style.mushroom_info}>Amanita</span></li>
-                            <li className={style.shroom_item}>Species: <span className={style.mushroom_info}>Muscária</span></li>
+                            <li className={style.shroom_item}>Genus: <span className={style.mushroom_info}>{shroom.genus}</span></li>
+                            <li className={style.shroom_item}>Species: <span className={style.mushroom_info}>{shroom.species}</span></li>
                             <li className={style.shroom_item}>Edible: <span className={style.mushroom_info}>No</span></li>
                             <li className={style.shroom_item}>Toxic: <span className={style.mushroom_info}>Yes</span></li>
                         </ul>
