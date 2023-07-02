0e2e1beb113cf854f4fa48a0054280b1f42f420e
LucasMonir <lucasmonir@hotmail.com>
2023-07-02 20:45:32 -0300

    finished (almost) the mushroom page!

--- src/components/layout/js/NavBar.js
+++ src/components/layout/js/NavBar.js
@@ -1,9 +1,13 @@
 import { Link } from 'react-router-dom';
-
+import { useLocation } from 'react-router-dom'
 import style from '../css/NavBar.module.css'
 import logo from '../../../img/amanita.png'
 
 function NavBar() {
+    const location = useLocation();
+    const activeStyle = {color: '#8b7aff'};
+    const inactiveStyle = { color: '#fff'};
+
     return (
         <nav className={style.navbar}>
             <Link to='/'>
@@ -12,16 +16,16 @@ function NavBar() {
 
             <ul className={style.list}>
                 <li className={style.item}>
-                    <Link to='/'>Home</Link>
+                    <Link to='/' style={location.pathname === '/' ? activeStyle : inactiveStyle}>Home</Link>
                 </li>
                 <li className={style.item}>
-                    <Link to='/cool'>Cool</Link>
+                    <Link to='/cool' style={location.pathname === '/cool' ? activeStyle : inactiveStyle}>Cool</Link>
                 </li>
                 <li className={style.item}>
-                    <Link to='/edible'>Edible</Link>
+                    <Link to='/edible' style={location.pathname === '/edible' ? activeStyle : inactiveStyle}>Edible</Link>
                 </li>
                 <li className={style.item}>
-                    <Link to='/toxic'>Toxic</Link>
+                    <Link to='/toxic' style={location.pathname === '/toxic' ? activeStyle : inactiveStyle}>Toxic</Link>
                 </li>
             </ul>
 
--- src/components/pages/js/Mushroom.js
+++ src/components/pages/js/Mushroom.js
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 
 function Mushroom({ id }) {
     const [shroom, setShroom] = useState([]);
-
+    
     useEffect(() => {
         if (!id) {
             fetch('http://localhost:4200/randomShroom',
@@ -18,14 +18,14 @@ function Mushroom({ id }) {
                 .then((x) => { setShroom(x)})
                 .catch(err => console.log(err))
         }
-    }, [])
+    }, [id])
 
     return (
         <section className={style.shroom_container}>
 
             <div className={style.shroom_data_container}>
                 <div className={style.shroom_image}>
-                    {/* <img src="https://cdn.britannica.com/90/236590-050-27422B8D/Close-up-of-mushroom-growing-on-field.jpg" alt="" /> */}
+                    <img src={shroom.img} alt="" />
                 </div>
                 <div className={style.shroom_data}>
                     <h1 className={style.mushroom_name}>{shroom.name}</h1>
@@ -33,12 +33,12 @@ function Mushroom({ id }) {
                         <ul>
                             <li className={style.shroom_item}>Genus: <span className={style.mushroom_info}>{shroom.genus}</span></li>
                             <li className={style.shroom_item}>Species: <span className={style.mushroom_info}>{shroom.species}</span></li>
-                            <li className={style.shroom_item}>Edible: <span className={style.mushroom_info}>No</span></li>
-                            <li className={style.shroom_item}>Toxic: <span className={style.mushroom_info}>Yes</span></li>
+                            <li className={style.shroom_item}>Edibility: <span className={style.mushroom_info}>{shroom.edible ? 'Edible' : 'Non-edible'}</span></li>
+                            <li className={style.shroom_item}>Toxicity: <span className={style.mushroom_info}>{shroom.toxic ? 'Toxic' : 'Non-toxic'}</span></li>
                         </ul>
 
                         <p className={style.mushroom_description}>
-                            {  shroom.description}
+                            {shroom.description}
                         </p>
                     </div>
                 </div>
@@ -48,4 +48,4 @@ function Mushroom({ id }) {
     );
 }
 
-export default Mushroom;
+export default Mushroom; 
