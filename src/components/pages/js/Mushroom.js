c3169ceab4e681660c82e177fa8043b16742ae3d
LucasMonir <lucasmonir@hotmail.com>
2023-07-04 09:00:55 -0300

    added some FA icons on the shroom page

--- src/components/pages/js/Mushroom.js
+++ src/components/pages/js/Mushroom.js
@@ -1,9 +1,11 @@
 import style from '../css/Mushroom.module.css'
 import { useEffect, useState } from "react";
+import { FaApple, FaBiohazard, FaSkull } from 'react-icons/fa'
+import { BiErrorAlt } from 'react-icons/bi'
 
 function Mushroom({ id }) {
     const [shroom, setShroom] = useState([]);
-    
+
     useEffect(() => {
         if (!id) {
             fetch('http://localhost:4200/randomShroom',
@@ -15,7 +17,7 @@ function Mushroom({ id }) {
 
                 })
                 .then(x => x.json())
-                .then((x) => { setShroom(x)})
+                .then((x) => { setShroom(x) })
                 .catch(err => console.log(err))
         }
     }, [id])
@@ -33,8 +35,8 @@ function Mushroom({ id }) {
                         <ul>
                             <li className={style.shroom_item}>Genus: <span className={style.mushroom_info}>{shroom.genus}</span></li>
                             <li className={style.shroom_item}>Species: <span className={style.mushroom_info}>{shroom.species}</span></li>
-                            <li className={style.shroom_item}>Edibility: <span className={style.mushroom_info}>{shroom.edible ? 'Edible' : 'Non-edible'}</span></li>
-                            <li className={style.shroom_item}>Toxicity: <span className={style.mushroom_info}>{shroom.toxic ? 'Toxic' : 'Non-toxic'}</span></li>
+                            <li className={style.shroom_item}>Edibility: {shroom.edible ? (<span className={style.mushroom_info}>Edible <FaApple /></span>) : (<span>Non-Edible<FaSkull /></span>)}</li>
+                            <li className={style.shroom_item}>Toxicity:  {shroom.toxic ? (<span className={style.mushroom_info}>Toxic <FaBiohazard /></span>) : (<span>Non-Toxic<BiErrorAlt /></span>)}</li>
                         </ul>
 
                         <p className={style.mushroom_description}>
